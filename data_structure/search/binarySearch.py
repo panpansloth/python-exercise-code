d2ce3a23606bfd36a4c1cd6def8fca8d965def2f
panpansloth <84489559+panpansloth@users.noreply.github.com>
2022-06-17 22:21:22 +0800

    Update binarySearch.py

--- data_structure/search/binarySearch.py
+++ data_structure/search/binarySearch.py
@@ -4,7 +4,7 @@ def binarySearch(alist, item):
     first = 0
     last = len(alist) - 1
     found = False
-    while not found and first < last:
+    while not found and first <= last:
         midpoint = (first + last) // 2
         if alist[midpoint] == item:
             found = True
